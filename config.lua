9fa3b0ddc5a5d1fd7a1043fe791e7c8485be6f23
STANLEY <88002173+SISTANLEY@users.noreply.github.com>
2021-07-26 20:16:41 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,43 @@
+Config = {}
+local Prefix = "rc!" -- GANTI SESUAI KEINGINAN KALIAN
+Config.ServerName = "ROLEPLAY COMUNITY"
+Config.DiscordServerID = 696543433854287913 -- GANTI ID SESUAI PILIHANMU
+Config.DiscordBotToken = "TOKEN BOT" -- MASUKAN BOT TOKEN KAMU DISINI
+Config.ApiLink = "MASUKANLINKAPI" -- MASUKAN JIKA KAMU INGIN MENGGUNAKAN API
+Config.ApiKey = ""
+Config.maxServerSlots = 1000
+Config.Roles = {
+    Example = {
+        roleID = "709806430869389442",
+        points = 10,
+        name = "OFFICIAL"
+    },
+
+    Example2 = {
+        roleID = "734092897506426931",
+        points = 15,
+        name = "ADMIMISTRATOR"
+    },
+}
+
+Config.Colors = {
+	"accent",
+	"good",
+	"warning",
+	"attention",
+}
+Config.Verifiying = Prefix .. "Please wait, Downloading content from Roleplay Comunity-database."
+Config.VerifiyingLauncher = Prefix .. "Please wait, Verifiying you entered through the launcher."
+Config.VerifiyingDiscord = Prefix .. "Please wait, Verifiying your Discord ID."
+Config.VerifiyingSteam = Prefix .. "Please wait, Verifiying your Steam."
+Config.VerifiyingQueue = Prefix .. "Please wait, Adding you to the queue."
+
+Config.NotWhitelisted = Prefix .. "Sorry, I didn't find you in our database."
+Config.NoDiscord = Prefix .. "Please make sure your Discord is open."
+Config.NoSteam = Prefix .. "Please make sure your Steam is open."
+Config.NoLauncher = Prefix .. "The server can only be accessed through its launcher."
+Config.Blacklisted = Prefix .. "You're blacklisted from the server, fuck off please."
+
+Config.Welcome = Prefix .. "Welcome Sir."
+Config.Error = Prefix .. "Error, Please try again later."
+Config.HandshakingWith = Prefix .. "Handshaking With Extra Leaks-Queue."
